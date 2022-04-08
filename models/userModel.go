2f63b207c17502947a80a4dbd323d586a0334b98
brunopereira03 <77056372+brunopereira03@users.noreply.github.com>
2022-04-08 14:19:30 +0100

    Fix User Struct

--- models/userModel.go
+++ models/userModel.go
@@ -1,11 +1,11 @@
 package models
 
 type User struct {
-	ID       		uint   `json:"id" gorm:"primaryKey"`
-	Username 		string `json:"username"`
-	Name     		string `json:"name"`
-	Email    		string `json:"email"`
-	Contact  		int    `json:"contact"`
-	Address  		string `json:"adress"`
-	Organization 	string `json:"organization"`
+	ID           uint   `json:"id" gorm:"primaryKey"`
+	Username     string `json:"username"`
+	Name         string `json:"name"`
+	Email        string `json:"email"`
+	Contact      int    `json:"contact"`
+	Address      string `json:"adress"`
+	Organization string `json:"organization"`
 }
