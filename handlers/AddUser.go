93014ddcfb7759abbdaa18301c161f3a6ca46c54
brunopereira03 <77056372+brunopereira03@users.noreply.github.com>
2022-04-28 12:28:59 +0100

    Post for new users to bridge

--- handlers/AddUser.go
+++ handlers/AddUser.go
@@ -1,56 +1,98 @@
 package handlers
 
 import (
-	"github.com/google/uuid"
+    "bytes"
+    "encoding/json"
+    "log"
+    "net/http"
 
-	"github.com/Food-to-Share/server/database"
-	"github.com/Food-to-Share/server/models"
-	"github.com/gin-gonic/gin"
+    "github.com/google/uuid"
+
+    "github.com/Food-to-Share/server/database"
+    "github.com/Food-to-Share/server/models"
+    "github.com/gin-gonic/gin"
 )
+
+type NewUser struct {
+    jid         string `json:"jid"`
+    displayName string `json:"name"`
+}
 
 func AddUser(c *gin.Context) {
 
-	db := database.GetDatabase()
+    db := database.GetDatabase()
+    httpposturl := "http://localhost:29318/_matrix/provision/v1/startUser"
 
-	var user models.User
-	var help models.Help
+    var user models.User
+    var help models.Help
+    println("hey")
 
-	err := c.ShouldBindJSON(&user)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"error": "Cannot bind JSON: " + err.Error(),
-		})
-		return
-	}
-	errHelp := c.ShouldBindJSON(&help)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"error": "Cannot bind JSON: " + errHelp.Error(),
-		})
-		return
-	}
+    err := c.ShouldBindJSON(&user)
+    if err != nil {
+        c.JSON(400, gin.H{
+            "error": "Cannot bind JSON: " + err.Error(),
+        })
+        return
+    }
+    println("hey1")
+    errHelp := c.ShouldBindJSON(&help)
+    if err != nil {
+        c.JSON(400, gin.H{
+            "error": "Cannot bind JSON: " + errHelp.Error(),
+        })
+        return
+    }
+    println("hey2")
 
-	user.ID = uuid.NewString()
-	help.ID = user.ID
+    user.ID = uuid.NewString()
+    help.ID = user.ID
+    println("hey3")
 
-	err = db.Create(&user).Error
+    err = db.Create(&user).Error
 
-	if err != nil {
-		c.JSON(400, gin.H{
-			"error": "Cannot create user: " + err.Error(),
-		})
-		return
-	}
+    if err != nil {
+        c.JSON(400, gin.H{
+            "error": "Cannot create user: " + err.Error(),
+        })
+        return
+    }
+    println("hey4")
 
-	err = db.Create(&help).Error
+    errHelp = db.Create(&help).Error
 
-	if err != nil {
-		c.JSON(400, gin.H{
-			"error": "Cannot create help: " + err.Error(),
-		})
-		return
-	}
+    if errHelp != nil {
+        c.JSON(400, gin.H{
+            "error": "Cannot create help: " + errHelp.Error(),
+        })
+        return
+    }
 
-	c.Status(204)
+    newUsers := NewUser{
+        jid:         user.ID,
+        displayName: user.Name,
+    }
+
+    data, err := json.Marshal(newUsers)
+    if err != nil {
+        log.Fatal(err)
+    }
+    reader := bytes.NewReader(data)
+
+    request, errorPost := http.NewRequest("POST", httpposturl, reader)
+    if errorPost != nil {
+        panic(errorPost)
+    }
+    client := &http.Client{}
+
+    response, error := client.Do(request)
+    if error != nil {
+        c.JSON(400, gin.H{
+            "error": "Cannot do request: " + error.Error(),
+        })
+        return
+    }
+    defer response.Body.Close()
+
+    c.Status(204)
 
 }
